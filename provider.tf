f0a46c812632749b58dfebc014942d5fb5fdc130
Sapna Rani <105926720+Sapna35@users.noreply.github.com>
2025-08-21 11:09:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,20 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "sapna-demobucket-200825"   
-    key    = "terraform.tfstate"
-    region = "eu-west-3"
-    encrypt = true
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "eu-west-3"
+  region = "eu-west-3" 
 }
-
